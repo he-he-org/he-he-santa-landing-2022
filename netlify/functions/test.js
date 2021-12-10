4c6eb0489807a8e243ffd4ed45d420e80da98032
Nikolai Mavrenkov <koluch@koluch.ru>
2021-12-10 23:35:40 +0300

    Add test function

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,11 @@
 exports.handler = async function(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({message: "Hello World", event, context})
+    body: JSON.stringify({
+      message: "Hello World",
+      event,
+      context,
+      env: process.env,
+    })
   };
 }
