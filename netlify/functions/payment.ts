24c5ce52f59398541d9b4aabc8ca68e9a91154d7
Nikolai Mavrenkov <koluch@koluch.ru>
2021-12-12 14:38:41 +0300

    Fix logging, remove old code

--- netlify/functions/payment.ts
+++ netlify/functions/payment.ts
@@ -93,101 +93,12 @@ const handler: Handler = async (event: Event, context: Context) => {
       }),
     };
   } catch (e) {
-    console.error(e)
+    console.log(e)
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Something has gone wrong on our side, sorry :(' }),
     };
   }
-
-
-
-  // try {
-  //   const response = await fetch(API_ENDPOINT);
-  //   const data = await response.json();
-  //   return { statusCode: 200, body: JSON.stringify({ data }) };
-  // } catch (error) {
-  //   console.log(error);
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify({ error: 'Failed fetching data' }),
-  //   };
-  // }
 };
-//
-// const { readDotEnv } = require('./common.js');
-// readDotEnv();
-//
-// const express = require('express');
-// const app = express();
-// console.log("process.env.STRIPE_SECRET_KEY", process.env.STRIPE_SECRET_KEY)
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-//
-// const ONE_TIME_PAYMENT_PRICE_ID = "price_1K5AQGG6m2c66h5uxnRaU2fS";
-// const SUBSCRIPTION_PRICE_ID = "price_1K2iItG6m2c66h5uTs2LL2P2";
-// const SUCCESS_URL = 'https://example.com/success';
-// const CANCEL_URL = 'https://example.com/cancel';
-// const PRODUCT_ID = "prod_Ki8aptzSldBWjf";
-//
-// function oneTimePayment(amount) {
-//   const inCents = Math.round(amount * 100);
-//   return {
-//     line_items: [
-//       {
-//         // price: ONE_TIME_PAYMENT_PRICE_ID,
-//         price_data: {
-//           currency: "USD",
-//           product: PRODUCT_ID,
-//           unit_amount: inCents,
-//         },
-//         quantity: 1,
-//       },
-//       {}
-//     ],
-//     mode: 'payment',
-//     success_url: SUCCESS_URL,
-//     cancel_url: CANCEL_URL,
-//   }
-// }
-//
-// function subscription(amount) {
-//   const inCents = Math.round(amount * 100);
-//   return {
-//     line_items: [
-//       {
-//         // price: SUBSCRIPTION_PRICE_ID,
-//         price_data: {
-//           currency: "USD",
-//           product: "prod_Ki8aptzSldBWjf",
-//           unit_amount: inCents,
-//           recurring: {
-//             interval: "month",
-//           },
-//         },
-//         quantity: 1,
-//         // quantity: inCents,
-//       },
-//       {}
-//     ],
-//     mode: 'subscription',
-//     success_url: SUCCESS_URL,
-//     cancel_url: CANCEL_URL,
-//   }
-// }
-//
-// app.get('/create-checkout-session', async (req, res) => {
-//   try {
-//     // http://localhost:4242/create-checkout-session
-//     const session = await stripe.checkout.sessions.create(oneTimePayment(3.42));
-//     // const session = await stripe.checkout.sessions.create(subscription(3.42));
-//
-//     res.redirect(303, session.url);
-//   } catch (e) {
-//     console.error(e)
-//     res.status(500).send(e.message ?? 'unknown error');
-//   }
-// });
-//
-// app.listen(4242, () => console.log(`Listening on port ${4242}!`));
 
 export { handler }
